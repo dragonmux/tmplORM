7848934c0c4a0ab142b3548f65429dc9eb2749c2
dx-mon <dx-mon@users.sourceforge.net>
2016-09-01 22:41:42 +0100

    Begun fleshing out the library structure

--- tmplORM.hxx
+++ tmplORM.hxx
@@ -1,11 +1,22 @@
 #ifndef tmplORM__HXX
 #define tmplORM__HXX
 
+#include <typestring/typestring.hh>
+
 namespace tmplORM
 {
+	using namespace irqus;
 
+	template<typename _tableName> struct model_t
+	{
+	};
 
-
-};
+	namespace types
+	{
+		template<typename _fieldName> struct field_t
+		{
+		};
+	}
+}
 
 #endif /*tmplORM__HXX*/
