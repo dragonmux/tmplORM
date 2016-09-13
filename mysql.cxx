745e9f27f445a9a62b6a0fdb80f2c97bb3850b8d
dx-mon <dx-mon@users.sourceforge.net>
2016-09-13 22:40:10 +0100

    Begun implementing a C++ binding of MySQL

---
+++ mysql.cxx
@@ -0,0 +1,19 @@
+#include "mysql.hxx"
+
+bool mySQLClient_t::connect(const char *const host, const uint32_t port, const char *const user, const char *const passwd) const noexcept
+	{ return valid() && mysql_real_connect(con, host, user, passwd, nullptr, port, nullptr, CLIENT_IGNORE_SIGPIPE) != nullptr; }
+
+bool mySQLClient_t::connect(const char *const unixSocket, const char *const user, const char *const passwd) const noexcept
+	{ return valid() && mysql_real_connect(con, nullptr, user, passwd, nullptr, 0, unixSocket, CLIENT_IGNORE_SIGPIPE) != nullptr; }
+
+bool mySQLClient_t::selectDB(const char *const db) const noexcept
+	{ return valid() && mysql_select_db(con, db) == 0; }
+
+uint32_t mySQLRow_t::numFields() noexcept
+{ return mysql_num_fields(result); }
+
+inline bool isNumber(const char x) noexcept
+	{ return x >= '0' && x <= '9'; }
+
+inline bool isMinus(const char x) noexcept
+	{ return x == '-'; }
---
+++ mysql.hxx
@@ -0,0 +1,88 @@
+#ifndef MYSQL__HXX
+#define MYSQL__HXX
+
+#include <stdint.h>
+#include <mysql.h>
+#include <utility>
+#include <memory>
+
+struct mySQLValue_t
+{
+private:
+	const char *const data;
+	const uint64_t len;
+	const uint8_t type;
+
+	//bool checkNumber(bool allowMinus) const noexcept;
+
+public:
+	mySQLValue_t(const char *const data, const uint64_t len, const uint8_t type) noexcept;
+	bool isNull() const noexcept { return data == nullptr; }
+
+	std::unique_ptr<char []>asSting() const;
+	bool asBool() const;
+	uint32_t asUint8() const;
+	int32_t asInt8() const;
+	uint32_t asUint16() const;
+	int32_t asInt16() const;
+	uint32_t asUint32() const;
+	int32_t asInt32() const;
+	uint64_t asUint64() const;
+	int64_t asInt64() const;
+
+	operator std::unique_ptr<char []>() const { return asString(); }
+	operator const char *() const { return asString().get(); }
+	explicit operator bool() const { return asBool(); }
+	operator uint8_t() const { return asUint8(); }
+	operator int8_t() const { return asInt8(); }
+	operator uint16_t() const { return asUint16(); }
+	operator int16_t() const { return asInt16(); }
+	operator uint32_t() const { return asUint32(); }
+	operator int32_t() const { return asInt32(); }
+	operator uint64_t() const { return asUint64(); }
+	operator int64_t() const { return asInt64(); }
+};
+
+struct mySQLRow_t
+{
+private:
+	MYSQL_RES *const result;
+	MYSQL_ROW row;
+	sql_ulong_t *const rowLengths;
+
+	mySQLRow_t(MYSQL_RES *const result) noexcept;
+	void fetch() noexcept;
+
+public:
+	constexpr mySQLRow_t() noexcept : result(nullptr), row(nullptr), rowLengths(nullptr) {}
+	~MySQLRow() noexcept;
+	uint32_t numFields() const noexcept;
+	bool next() noexcept;
+	mySQLValue_t operator [](const uint32_t idx) const noexcept;
+	bool valid() const noexcept;
+
+	mySQLRow_t(const mySQLRow_t &) = delete;
+};
+
+struct mySQLClient_t
+{
+private:
+	MYSQL *const con;
+	bool haveConnection;
+	int result;
+
+	constexpr mySQLClient_t() noexcept {}
+
+public:
+	bool connect(const char *host, uint32_t port, const char *user, const char *passwd) const noexcept;
+	bool connect(const char *unixSocket, const char *user, const char *passwd) const noexcept;
+	bool selectDB(const char *db) const noexcept;
+	bool query(const char *queryStmt, ...) MySQL_FORMAT_ARGS(2, 3);
+	mySQLResult_t queryResult() noexcept;
+	uint32_t errorNum() const noexcept;
+	const char *error() const noexcept;
+};
+
+extern mySQLClient_t &database;
+
+#endif /*MYSQL__HXX*/
