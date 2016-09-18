7be8caf291612d76e5e8cb21d3953a5c762f226e
dx-mon <dx-mon@users.sourceforge.net>
2016-09-18 14:46:35 +0100

    Marked the current implementation of the field listing code, as being specifically for selects and select-like queries

--- tmplORM.mysql.hxx
+++ tmplORM.mysql.hxx
@@ -17,20 +17,20 @@ namespace tmplORM
 			{ using value = tycat<backtick<fieldName>, ts(", ")>; };
 		template<typename fieldName, typename T> struct fieldName_t<1, type_t<fieldName, T>> { using value = backtick<fieldName>; };
 
-		template<size_t N> struct fieldList__t
+		template<size_t N> struct selectList__t
 		{
 			template<typename fieldName, typename T> constexpr static auto value(const type_t<fieldName, T> &) ->
 				typename fieldName_t<N, type_t<fieldName, T>>::value;
 		};
-		template<size_t N, typename T> using fieldList__ = decltype(fieldList__t<N>::value(T()));
+		template<size_t N, typename T> using selectList__ = decltype(selectList__t<N>::value(T()));
 
-		template<size_t N, typename field, typename... fields> struct fieldList_t
-			{ using value = tycat<fieldList__<N, field>, typename fieldList_t<N - 1, fields...>::value>; };
-		template<typename field> struct fieldList_t<1, field> { using value = fieldList__<1, field>; };
+		template<size_t N, typename field, typename... fields> struct selectList_t
+			{ using value = tycat<selectList__<N, field>, typename selectList_t<N - 1, fields...>::value>; };
+		template<typename field> struct selectList_t<1, field> { using value = selectList__<1, field>; };
 
-		template<typename... fields> using fieldList = typename fieldList_t<sizeof...(fields), fields...>::value;
+		template<typename... fields> using selectList = typename selectList_t<sizeof...(fields), fields...>::value;
 
-		template<typename tableName, typename... fields> using select__ = tycat<ts("SELECT "), fieldList<fields...>,
+		template<typename tableName, typename... fields> using select__ = tycat<ts("SELECT "), selectList<fields...>,
 			ts(" FROM "), backtick<tableName>, ts(";")>;
 		template<typename tableName, typename... fields> bool select_(const model_t<tableName, fields...> &) noexcept
 		{
