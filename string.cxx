1864aa0b6b8c4f1741cd11cee55d9d8c5511652c
dx-mon <dx-mon@users.sourceforge.net>
2016-09-29 23:57:48 +0100

    Implemented the functions needed to produce a count of the number of UTF-16 code units needed to represent a UTF-8 string

--- string.cxx
+++ string.cxx
@@ -20,3 +20,56 @@ std::unique_ptr<const char []> vaFormatString(const char *format, va_list args) 
 	vsprintf(ret.get(), format, args);
 	return std::unique_ptr<const char []>(ret.release());
 }
+
+inline bool isMultiValid() noexcept { return true; }
+template<typename... values_t> inline bool isMultiValid(const char c, values_t ...values) noexcept
+	{ return (c & 0xC0) == 0x80 && isMultiValid(values...); }
+
+inline char safeIndex(const char *const str, const size_t index, const size_t len) noexcept
+{
+	if (index >= len)
+		return -1;
+	return str[index];
+}
+
+size_t countUnits(const char *const str) noexcept
+{
+	const size_t len = strlen(str);
+	size_t count = 0;
+	for (size_t i = 0; i < len; ++i)
+	{
+		const char byteA = str[i];
+		// Multiple code unit encoded character?
+		if (byteA & 0x80)
+		{
+			const char byteB = safeIndex(str, ++i, len);
+			if ((byteA & 0x60) == 0x40)
+			{
+				// 2 code units.. check that the second unit is valid and return 0 if not.
+				if (!isMultiValid(byteB))
+					return 0;
+			}
+			else if ((byteA & 0x70) == 0x60)
+			{
+				// 3 code units.. check that the second and third units are valid and return 0 if not
+				if (!isMultiValid(byteB, safeIndex(str, ++i, len)))
+					return 0;
+				// Also check that the whole only contributes one UTF-16 unit - if it does not, pre-increment count
+				if (byteA & 0x0F)
+					++count;
+			}
+			else if ((byteA & 0x78) == 0x70)
+			{
+				// 4 code units.. check that the second, third and fourth unit is valid
+				if (!isMultiValid(byteB, safeIndex(str, i + 1, len), safeIndex(str, i + 2, len)))
+					return 0;
+				i += 2;
+				++count;
+			}
+			else
+				return 0;
+		}
+		++count;
+	}
+	return count;
+}
