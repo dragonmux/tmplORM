8bb60f5363ce47353967b1aebf17add00f945231
dx-mon <dx-mon@users.sourceforge.net>
2016-10-08 21:46:47 +0100

    Implemented a few more models

--- test/models.hxx
+++ test/models.hxx
@@ -40,6 +40,37 @@ namespace models
 		types::nullable_t<types::unicode_t<ts("Phone"), 24>>
 	> { };
 
+	struct region_t : public model_t<ts("Regions"),
+		types::autoInc_t<types::primary_t<types::int32_t<ts("RegionID")>>>, types::unicode_t<ts("RegionDescription"), 50>
+	> { };
+
+	struct territory_t : public model_t<ts("Territories"),
+		types::primary_t<types::unicode_t<ts("TerritoryID"), 20>>, types::unicode_t<ts("TerritoryDescription"), 50>,
+		types::int32_t<ts("RegionID")>
+	> { };
+
+	struct employee_t : public model_t<ts("Employees"),
+		types::autoInc_t<types::primary_t<types::int32_t<ts("EmployeeID")>>>, types::unicode_t<ts("LastName"), 20>,
+		types::unicode_t<ts("FirstName"), 10>, types::nullable_t<types::unicode_t<ts("Title"), 30>>,
+		types::nullable_t<types::unicode_t<ts("TitleOfCourtesy"), 25>>, types::nullable_t<types::dateTime_t<ts("BirthDate")>>,
+		types::nullable_t<types::dateTime_t<ts("HireDate")>>, types::nullable_t<types::unicode_t<ts("Address"), 60>>,
+		types::nullable_t<types::unicode_t<ts("City"), 15>>, types::nullable_t<types::unicode_t<ts("Region"), 15>>,
+		types::nullable_t<types::unicode_t<ts("PostalCode"), 10>>, types::nullable_t<types::unicode_t<ts("Country"), 15>>,
+		types::nullable_t<types::unicode_t<ts("HomePhone"), 24>>, types::nullable_t<types::unicode_t<ts("Extension"), 4>>,
+		/*types::nullable_t<types::binary_t<ts("Photo")>>, */types::nullable_t<types::unicodeText_t<ts("Notes")>>,
+		types::nullable_t<types::int32_t<ts("ReportsTo")>>, types::nullable_t<types::unicode_t<ts("PhotoPath"), 255>>
+	> { };
+
+	struct order_t : public model_t<ts("Orders"),
+		types::autoInc_t<types::primary_t<types::int32_t<ts("OrderID")>>>, types::nullable_t<types::unicode_t<ts("CustomerID"), 5>>,
+		types::nullable_t<types::int32_t<ts("EmployeeID")>>, types::nullable_t<types::dateTime_t<ts("OrderDate")>>,
+		types::nullable_t<types::dateTime_t<ts("RequiredDate")>>, types::nullable_t<types::dateTime_t<ts("ShippedDate")>>,
+		types::nullable_t<types::int32_t<ts("ShipVia")>>,// types::nullable_t<types:: <ts("Freight")>>,
+		types::nullable_t<types::unicode_t<ts("ShipName"), 40>>, types::nullable_t<types::unicode_t<ts("ShipAddress"), 60>>,
+		types::nullable_t<types::unicode_t<ts("ShipCity"), 15>>, types::nullable_t<types::unicode_t<ts("ShipRegion"), 15>>,
+		types::nullable_t<types::unicode_t<ts("ShipPostalCode"), 10>>, types::nullable_t<types::unicode_t<ts("ShipCountry"), 15>>
+	> { };
+
 	struct demographic_t : public model_t<ts("CustomerDemographics"),
 		types::primary_t<types::unicode_t<ts("CustomerTypeID"), 10>>, types::nullable_t<types::unicodeText_t<ts("CustomerDesc")>>
 	> { };
