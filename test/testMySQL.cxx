b5c2364049033c69ce1ea2175bb8dbf346ccccb6
DX-MON <dx-mon@users.sourceforge.net>
2017-04-20 10:05:56 +0100

    Implemented the first of the mySQLValue_t tests

--- test/testMySQL.cxx
+++ test/testMySQL.cxx
@@ -1,17 +1,32 @@
 #include <crunch++.h>
 #include <mysql.hxx>
 
+using namespace tmplORM::mysql::driver;
+
+class testMySQLValue final : public testsuit
+{
+public:
+	void testString()
+	{
+		assertTrue(mySQLValue_t(nullptr, 0, MYSQL_TYPE_VARCHAR).isNull());
+	}
+
+	void registerTests() final override
+	{
+		CXX_TEST(testString)
+	}
+};
+
 class testMySQL final : public testsuit
 {
 public:
 	void registerTests() final override
 	{
-		//CXX_TEST(testValidity)
 	}
 };
 
 CRUNCH_API void registerCXXTests() noexcept;
 void registerCXXTests() noexcept
 {
-	registerTestClasses<testMySQL>();
+	registerTestClasses<testMySQLValue, testMySQL>();
 }
