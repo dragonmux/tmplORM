026a372b3395c4a243562a551b7fd3043e5b7236
DX-MON <dx-mon@users.sourceforge.net>
2017-04-20 11:33:24 +0100

    Expanded the tests for mySQLValue::asString() to encompas more of the code in question

--- test/testMySQL.cxx
+++ test/testMySQL.cxx
@@ -5,10 +5,27 @@ using namespace tmplORM::mysql::driver;
 
 class testMySQLValue final : public testsuit
 {
+	template<typename T> void assertUNull(std::unique_ptr<T> &value)
+		{ assertNull(value.get()); }
+	template<typename T> void assertUNotNull(std::unique_ptr<T> &value)
+		{ assertNotNull(value.get()); }
+
 public:
 	void testString()
 	{
-		assertTrue(mySQLValue_t(nullptr, 0, MYSQL_TYPE_VARCHAR).isNull());
+		const std::array<char, 23> testData =
+		{
+			'T', 'h', 'i', 's', ' ', 'i', 's', ' ',
+			'\x00', '\xFF', ' ', 'o', 'n', 'l', 'y', ' ',
+			'a', ' ', 't', 'e', 's', 't', '\0'
+		};
+		auto nullStr = mySQLValue_t(nullptr, 0, MYSQL_TYPE_VARCHAR).asString();
+		assertUNull(nullStr);
+		auto nullValue = mySQLValue_t(testData.data(), 0, MYSQL_TYPE_NULL).asString();
+		assertUNull(nullValue);
+		auto testStr = mySQLValue_t(testData.data(), testData.size(), MYSQL_TYPE_VARCHAR).asString();
+		assertUNotNull(testStr);
+		assertEqual(testStr.get(), testData.data(), testData.size());
 	}
 
 	void registerTests() final override
