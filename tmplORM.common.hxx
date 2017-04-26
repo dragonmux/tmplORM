16f59dd076c9a3ab395cba22390249e6d0ebae2d
DX-MON <dx-mon@users.sourceforge.net>
2017-04-26 21:58:26 +0100

    Rewritten the entire bind engine, including adding support for calculating the length of the field being passed in to the bind

--- tmplORM.common.hxx
+++ tmplORM.common.hxx
@@ -80,7 +80,7 @@ inline namespace common
 	{
 		constexpr static size_t index = idx - 1;
 
-		template<typename result_t> static void bind(std::tuple<fields_t...> &fields, result_t &result) noexcept
+		template<typename result_t> static void bind(std::tuple<fields_t...> &fields, const result_t &result) noexcept
 		{
 			bindSelect_t<index, fields_t...>::bind(fields, result);
 			std::get<index>(fields) = result[index];
@@ -88,40 +88,47 @@ inline namespace common
 	};
 
 	template<typename... fields> struct bindSelect_t<0, fields...>
-		{ template<typename result_t> static void bind(std::tuple<fields...> &, result_t &) { } };
+		{ template<typename result_t> static void bind(std::tuple<fields...> &, const result_t &) { } };
 	template<typename... fields> using bindSelect = bindSelect_t<sizeof...(fields), fields...>;
 
-	template<size_t bindIndex, typename field_t> struct bindField_t
-	{
-		template<typename query_t, typename std::enable_if<!field_t::nullable, void *>::type = nullptr>
-			static void bind(const field_t &field, query_t &query) noexcept
-		{ query.bind(bindIndex, field.value()); }
+	template<typename field_t> constexpr fieldLength_t fieldLength(const field_t &) noexcept { return {0, 0}; }
+	template<typename fieldName, size_t length> fieldLength_t fieldLength(const unicode_t<fieldName, length> &field) noexcept
+		{ return {field.length(), length}; }
+	template<typename fieldName> fieldLength_t fieldLength(const unicodeText_t<fieldName> &field) noexcept
+		{ return {field.length(), (1 << 16) - 1}; }
+	template<size_t bindIndex, typename field_t, bool = field_t::nullable> struct bindField_t;
 
-		template<typename query_t, typename std::enable_if<field_t::nullable, void *>::type = nullptr>
-			static void bind(const field_t &field, query_t &query) noexcept
+	template<size_t bindIndex, typename field_t> struct bindField_t<bindIndex, field_t, false>
+	{
+		template<typename query_t> static void bind(const field_t &field, query_t &query) noexcept
+			{ query.bind(bindIndex, field.value(), fieldLength(field)); }
+	};
+
+	template<size_t bindIndex, typename field_t> struct bindField_t<bindIndex, field_t, true>
+	{
+		template<typename query_t> static void bind(const field_t &field, query_t &query) noexcept
 		{
 			if (field.isNull())
-				query.template bind<typename decltype(field)::type>(bindIndex, nullptr);
+				query.bind<typename field_t::type>(bindIndex, nullptr, fieldLength(field_t()));
 			else
-				query.bind(bindIndex, field.value());
+				query.bind(bindIndex, field.value(), fieldLength(field));
 		}
 	};
 
-	template<size_t idx, size_t bindIdx, typename... fields_t> struct bindInsert_t
+	template<size_t index, size_t bindIdx, typename... fields_t> struct bindInsert_t
 	{
-		constexpr static size_t index = idx - 1;
-		constexpr static size_t bindIndex = bindIdx - 1;
+		constexpr static const size_t bindIndex = bindIdx - 1;
 
 		template<typename fieldName, typename T, typename field_t, typename query_t>
 			static void bindField(const type_t<fieldName, T> &, const field_t &field, const std::tuple<fields_t...> &fields, query_t &query) noexcept
 		{
-			bindInsert_t<index, bindIndex, fields_t...>::bind(fields, query);
-			bindField_t<bindIdx, field_t>::bind(field, query);
+			bindInsert_t<index - 1, bindIndex, fields_t...>::bind(fields, query);
+			bindField_t<bindIndex, field_t>::bind(field, query);
 		}
 
 		template<typename T, typename field_t, typename query_t>
 			static void bindField(const autoInc_t<T> &, const field_t &, const std::tuple<fields_t...> &fields, query_t &query) noexcept
-		{ bindInsert_t<index, bindIdx, fields_t...>::bind(fields, query); }
+		{ bindInsert_t<index - 1, bindIdx, fields_t...>::bind(fields, query); }
 
 		template<typename query_t> static void bind(const std::tuple<fields_t...> &fields, query_t &query) noexcept
 		{
@@ -130,9 +137,9 @@ inline namespace common
 		}
 	};
 
-	template<size_t index, typename... fields_t> struct bindInsert_t<index, 0, fields_t...>
-		{ template<typename query_t> static void bind(const std::tuple<fields_t...> &, query_t &) { } };
-	template<typename... fields> using bindInsert = bindInsert_t<sizeof...(fields), countInsert_t<fields...>::count, fields...>;
+	template<size_t index, typename... fields> struct bindInsert_t<index, 0, fields...>
+		{ template<typename query_t> static void bind(const std::tuple<fields...> &, query_t &) noexcept { } };
+	template<typename... fields> using bindInsert = bindInsert_t<sizeof...(fields) - 1, countInsert_t<fields...>::count, fields...>;
 
 	template<size_t idx, size_t bindIdx, size_t keyBindIdx, typename... fields_t> struct bindUpdate_t
 	{
