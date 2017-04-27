1da54d5425a7de1547eda1af4a6cfa94370de064
DX-MON <dx-mon@users.sourceforge.net>
2017-04-27 23:26:55 +0100

    Implemented the types needed to properly and correctly handle date and time in the ORM - to and from string form, to and from the database held form, transparently

--- tmplORM.types.hxx
+++ tmplORM.types.hxx
@@ -3,4 +3,79 @@
 
 #include "conversions.hxx"
 
+namespace tmplORM
+{
+	namespace types
+	{
+		inline namespace baseTypes
+		{
+			struct ormDate_t
+			{
+			protected:
+				uint16_t _year;
+				uint16_t _month;
+				uint16_t _day;
+
+			public:
+				constexpr ormDate_t() noexcept : _year(0), _month(0), _day(0) { }
+				constexpr ormDate_t(const uint16_t year, const uint16_t month, const uint16_t day) :
+					_year(year), _month(month), _day(day) { }
+
+				constexpr uint16_t year() const noexcept { return _year; }
+				constexpr uint16_t month() const noexcept { return _month; }
+				constexpr uint16_t day() const noexcept { return _day; }
+			};
+
+			struct ormDateTime_t final : public ormDate_t
+			{
+			private:
+				uint16_t _hour;
+				uint16_t _minute;
+				uint16_t _second;
+				uint32_t _nanoSecond;
+
+				size_t power10(const size_t power) const noexcept
+				{
+					size_t ret = 1;
+					for (size_t i = 0; i < power; ++i)
+						ret *= 10;
+					return ret;
+				}
+
+			public:
+				constexpr ormDateTime_t() noexcept : ormDate_t(), _hour(0), _minute(0), _second(0), _nanoSecond(0) { }
+				constexpr ormDateTime_t(const uint16_t year, const uint16_t month, const uint16_t day, const uint16_t hour,
+					const uint16_t minute, const uint16_t second, const uint32_t nanoSecond) :
+					ormDate_t(year, month, day), _hour(hour), _minute(minute), _second(second), _nanoSecond(nanoSecond) { }
+
+				constexpr uint16_t hour() const noexcept { return _hour; }
+				constexpr uint16_t minute() const noexcept { return _minute; }
+				constexpr uint16_t second() const noexcept { return _second; }
+				constexpr uint16_t nanoSecond() const noexcept { return _nanoSecond; }
+
+				ormDateTime_t(const char *dateTime) noexcept : ormDateTime_t()
+				{
+					_year = toInt_t<uint16_t>(dateTime, 4);
+					dateTime += 5;
+					_month = toInt_t<uint16_t>(dateTime, 2);
+					dateTime += 3;
+					_day = toInt_t<uint16_t>(dateTime, 2);
+					dateTime += 3;
+					_hour = toInt_t<uint16_t>(dateTime, 2);
+					dateTime += 3;
+					_minute = toInt_t<uint16_t>(dateTime, 2);
+					dateTime += 3;
+					_second = toInt_t<uint16_t>(dateTime, 2);
+					dateTime += 3;
+					toInt_t<uint32_t> nanoSeconds(dateTime);
+					if (nanoSeconds.length() <= 9)
+						_nanoSecond = nanoSeconds * power10(9 - nanoSeconds.length());
+					else
+						_nanoSecond = nanoSeconds / power10(nanoSeconds.length() - 9);
+				}
+			};
+		}
+	}
+}
+
 #endif /*tmplORM_TYPES__HXX*/
