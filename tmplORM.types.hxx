b52073dccb6ba592cdca5680c107ea6c7a7ca253
DX-MON <dx-mon@users.sourceforge.net>
2017-05-09 22:45:07 +0100

    Implemented the proper conversion initialiser for ormDate_t

--- tmplORM.types.hxx
+++ tmplORM.types.hxx
@@ -18,12 +18,21 @@ namespace tmplORM
 
 			public:
 				constexpr ormDate_t() noexcept : _year(0), _month(0), _day(0) { }
-				constexpr ormDate_t(const uint16_t year, const uint16_t month, const uint16_t day) :
+				constexpr ormDate_t(const uint16_t year, const uint16_t month, const uint16_t day) noexcept :
 					_year(year), _month(month), _day(day) { }
 
 				constexpr uint16_t year() const noexcept { return _year; }
 				constexpr uint16_t month() const noexcept { return _month; }
 				constexpr uint16_t day() const noexcept { return _day; }
+
+				ormDate_t(const char *date) noexcept : ormDate_t()
+				{
+					_year = toInt_t<uint16_t>(date, 4);
+					date += 5;
+					_month = toInt_t<uint16_t>(date, 2);
+					date += 3;
+					_day = toInt_t<uint16_t>(date, 2);
+				}
 			};
 
 			struct ormDateTime_t final : public ormDate_t
@@ -44,23 +53,18 @@ namespace tmplORM
 
 			public:
 				constexpr ormDateTime_t() noexcept : ormDate_t(), _hour(0), _minute(0), _second(0), _nanoSecond(0) { }
-				constexpr ormDateTime_t(const uint16_t year, const uint16_t month, const uint16_t day, const uint16_t hour,
-					const uint16_t minute, const uint16_t second, const uint32_t nanoSecond) :
+				constexpr ormDateTime_t(const uint16_t year, const uint16_t month, const uint16_t day,
+					const uint16_t hour, const uint16_t minute, const uint16_t second, const uint32_t nanoSecond) :
 					ormDate_t(year, month, day), _hour(hour), _minute(minute), _second(second), _nanoSecond(nanoSecond) { }
 
 				constexpr uint16_t hour() const noexcept { return _hour; }
 				constexpr uint16_t minute() const noexcept { return _minute; }
 				constexpr uint16_t second() const noexcept { return _second; }
-				constexpr uint16_t nanoSecond() const noexcept { return _nanoSecond; }
+				constexpr uint32_t nanoSecond() const noexcept { return _nanoSecond; }
 
-				ormDateTime_t(const char *dateTime) noexcept : ormDateTime_t()
+				ormDateTime_t(const char *dateTime) noexcept : ormDate_t(dateTime), _hour(0), _minute(0), _second(0), _nanoSecond(0)
 				{
-					_year = toInt_t<uint16_t>(dateTime, 4);
-					dateTime += 5;
-					_month = toInt_t<uint16_t>(dateTime, 2);
-					dateTime += 3;
-					_day = toInt_t<uint16_t>(dateTime, 2);
-					dateTime += 3;
+					dateTime += 11;
 					_hour = toInt_t<uint16_t>(dateTime, 2);
 					dateTime += 3;
 					_minute = toInt_t<uint16_t>(dateTime, 2);
