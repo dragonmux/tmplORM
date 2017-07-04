d3f0dd89dcbc452e8eb437318b8c64af197720e3
dx-mon <dx-mon@users.sourceforge.net>
2017-07-05 00:13:42 +0100

    Fixed the excess of errors being generated during unsupported builds

--- tmplORM.extern.hxx
+++ tmplORM.extern.hxx
@@ -8,6 +8,7 @@
 		#define tmplORM_DEFAULT_VISIBILITY __attribute__((visibility("default")))
 	#else
 		#error Unsupported version of GCC - Must use GCC-5 or higher to correctly compile tmplORM.
+		#define tmplORM_DEFAULT_VISIBILITY
 	#endif
 	#define tmplORM_API tmplORM_DEFAULT_VISIBILITY
 	#define tmplORM_FNAPI extern tmplORM_API
