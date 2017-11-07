33abf9a7eca2787f8c58d4b5576ea3cc7b49afed
dx-mon <dx-mon@users.sourceforge.net>
2017-11-07 21:54:26 +0000

    Added an allowance for clang >= 4 to see if the compiler is ready enough (IE, not completely and utterly smashed out of it's head broken) for tmplORM

--- tmplORM.extern.hxx
+++ tmplORM.extern.hxx
@@ -4,7 +4,7 @@
 #ifdef _MSC_VER
 	// TODO: Define me.
 #else
-	#if __GNUC__ > 4
+	#if __GNUC__ > 4 || __clang_major__ > 3
 		#define tmplORM_DEFAULT_VISIBILITY __attribute__((visibility("default")))
 	#else
 		#error Unsupported version of GCC - Must use GCC-5 or higher to correctly compile tmplORM.
