f25d72c7e5bd60a549545916cb25795654a16b22
dx-mon <dx-mon@users.sourceforge.net>
2017-11-12 17:29:52 +0000

    Implemented tests for the hex conversions

--- test/testConversions.cxx
+++ test/testConversions.cxx
@@ -360,6 +360,168 @@ public:
 	}
 };
 
+struct testHexToInt_t final : testsuit
+{
+public:
+	void testUint8_t()
+	{
+		testHexToUint8(*this,
+			{
+				{0, ""},
+				{0, "0"},
+				{0, "00"},
+				{127, "7F"},
+				{128, "80"},
+				{255, "FF"}
+			}
+		);
+	}
+
+	void testInt8_t()
+	{
+		testHexToInt8(*this,
+			{
+				{0, ""},
+				{0, "00"},
+				{127, "7F"},
+				{-1, "FF"},
+				{-127, "81"},
+				{-128, "80"}
+			}
+		);
+	}
+
+	void testUint16_t()
+	{
+		testHexToUint16(*this,
+			{
+				{0, ""},
+				{0, "00"},
+				{256, "0100"},
+				{32767, "7FFF"},
+				{32768, "8000"},
+				{65535, "FFFF"}
+			}
+		);
+	}
+
+	void testInt16_t()
+	{
+		testHexToInt16(*this,
+			{
+				{0, ""},
+				{0, "00"},
+				{128, "80"},
+				{255, "FF"},
+				{256, "0100"},
+				{32767, "7FFF"},
+				{-1, "FFFF"},
+				{-255, "FF01"},
+				{-256, "FF00"},
+				{-32767, "8001"},
+				{-32768, "8000"}
+			}
+		);
+	}
+
+	void testUint32_t()
+	{
+		testHexToUint32(*this,
+			{
+				{0, ""},
+				{0, "00"},
+				{65536, "00010000"},
+				{2147483647, "7FFFFFFF"},
+				{2147483648, "80000000"},
+				{4294967295, "FFFFFFFF"}
+			}
+		);
+	}
+
+	void testInt32_t()
+	{
+		testHexToInt32(*this,
+			{
+				{0, ""},
+				{0, "00"},
+				{32768, "8000"},
+				{65535, "FFFF"},
+				{65536, "00010000"},
+				{2147483647, "7FFFFFFF"},
+				{-1, "FFFFFFFF"},
+				{-65535, "FFFF0001"},
+				{-65536, "FFFF0000"},
+				{-2147483647, "80000001"},
+				{-2147483648, "80000000"}
+			}
+		);
+	}
+
+	void testUint64_t()
+	{
+		testHexToUint64(*this,
+			{
+				{0, ""},
+				{0, "00"},
+				{4294967296, "0000000100000000"},
+				{140737488355327, "00007FFFFFFFFFFF"},
+				{140737488355328, "0000800000000000"},
+				{9223372036854775807, "7FFFFFFFFFFFFFFF"},
+				{UINT64_C(9223372036854775808), "8000000000000000"},
+				{UINT64_C(18446744073709551615), "FFFFFFFFFFFFFFFF"}
+			}
+		);
+	}
+
+	void testInt64_t()
+	{
+		testHexToInt64(*this,
+			{
+				{0, ""},
+				{0, "00"},
+				{2147483648, "80000000"},
+				{4294967295, "FFFFFFFF"},
+				{4294967296, "0000000100000000"},
+				{140737488355327, "00007FFFFFFFFFFF"},
+				{140737488355328, "0000800000000000"},
+				{9223372036854775807, "7FFFFFFFFFFFFFFF"},
+				{-1, "FFFFFFFFFFFFFFFF"},
+				{-4294967295, "FFFFFFFF00000001"},
+				{-4294967296, "FFFFFFFF00000000"},
+				{-140737488355327, "FFFF800000000001"},
+				{-140737488355328, "FFFF800000000000"},
+				{-9223372036854775807, "8000000000000001"},
+				{INT64_C(-9223372036854775808), "8000000000000000"}
+			}
+		);
+	}
+
+	void testShouldFail()
+	{
+		testHexShouldFail(*this,
+			{
+				"-", "+", "-a", "-A",
+				"-0", "-$", "$", "*",
+				"/", "\x01", " ", "\t",
+				"\n", "\r", "^", "&"
+			}
+		);
+	}
+
+	void registerTests() final override
+	{
+		CXX_TEST(testUint8_t)
+		CXX_TEST(testInt8_t)
+		CXX_TEST(testUint16_t)
+		CXX_TEST(testInt16_t)
+		CXX_TEST(testUint32_t)
+		CXX_TEST(testInt32_t)
+		CXX_TEST(testUint64_t)
+		CXX_TEST(testInt64_t)
+		CXX_TEST(testShouldFail)
+	}
+};
+
 struct testSwapBytes_t final : testsuit
 {
 public:
@@ -400,5 +562,5 @@ CRUNCH_API void registerCXXTests() noexcept;
 void registerCXXTests() noexcept
 {
 	registerTestClasses<testFromInt_t, testDecToInt_t,
-		testSwapBytes_t>();
+		testHexToInt_t, testSwapBytes_t>();
 }
