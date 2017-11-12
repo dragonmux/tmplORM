ebe7ac18269723f63570bfbf9b0d67746c8ef82b
dx-mon <dx-mon@users.sourceforge.net>
2017-11-12 01:31:17 +0000

    Implemented the final 6 conversions tests elaborations

--- test/conversions.cxx
+++ test/conversions.cxx
@@ -22,3 +22,15 @@ void testUint8(testsuit &suite, const testOk_t<uint8_t> tests)
 	{ testFromInt_t<uint8_t> tester; tester.testConversions(suite, tests); }
 void testInt8(testsuit &suite, const testOk_t<int8_t> tests)
 	{ testFromInt_t<int8_t> tester; tester.testConversions(suite, tests); }
+void testUint16(testsuit &suite, const testOk_t<uint16_t> tests)
+	{ testFromInt_t<uint16_t> tester; tester.testConversions(suite, tests); }
+void testInt16(testsuit &suite, const testOk_t<int16_t> tests)
+	{ testFromInt_t<int16_t> tester; tester.testConversions(suite, tests); }
+void testUint32(testsuit &suite, const testOk_t<uint32_t> tests)
+	{ testFromInt_t<uint32_t> tester; tester.testConversions(suite, tests); }
+void testInt32(testsuit &suite, const testOk_t<int32_t> tests)
+	{ testFromInt_t<int32_t> tester; tester.testConversions(suite, tests); }
+void testUint64(testsuit &suite, const testOk_t<uint64_t> tests)
+	{ testFromInt_t<uint64_t> tester; tester.testConversions(suite, tests); }
+void testInt64(testsuit &suite, const testOk_t<int64_t> tests)
+	{ testFromInt_t<int64_t> tester; tester.testConversions(suite, tests); }
--- test/testConversions.hxx
+++ test/testConversions.hxx
@@ -12,5 +12,11 @@ using testFailStr_t = std::vector<const char *const>;
 
 extern void testUint8(testsuit &suit, const testOk_t<uint8_t> tests);
 extern void testInt8(testsuit &suit, const testOk_t<int8_t> tests);
+extern void testUint16(testsuit &suit, const testOk_t<uint16_t> tests);
+extern void testInt16(testsuit &suit, const testOk_t<int16_t> tests);
+extern void testUint32(testsuit &suit, const testOk_t<uint32_t> tests);
+extern void testInt32(testsuit &suit, const testOk_t<int32_t> tests);
+extern void testUint64(testsuit &suit, const testOk_t<uint64_t> tests);
+extern void testInt64(testsuit &suit, const testOk_t<int64_t> tests);
 
 #endif /*TEST_CONVERSIONS__HXX*/
