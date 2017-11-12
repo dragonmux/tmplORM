775faef5247974ecfbcb272ad3c83388d59786ff
dx-mon <dx-mon@users.sourceforge.net>
2017-11-12 14:57:23 +0000

    Implemented the start of a set of tests for toInt_t<> - starting with the decimal conversions

--- test/conversions.cxx
+++ test/conversions.cxx
@@ -35,6 +35,39 @@ void testFromUint64(testsuit &suite, const testOk_t<uint64_t> tests)
 void testFromInt64(testsuit &suite, const testOk_t<int64_t> tests)
 	{ testFromInt_t<int64_t> tester; tester.testConversions(suite, tests); }
 
+template<typename int_t> struct testToInt_t
+{
+private:
+	using toInt = toInt_t<int_t>;
+
+public:
+	void testIntConversions(testsuit &suite, const testOk_t<int_t> &tests)
+	{
+		for (const auto &test : tests)
+		{
+			int_t value = toInt{test.second};
+			suite.assertEqual(value, test.first);
+		}
+	}
+};
+
+void testToUint8(testsuit &suite, const testOk_t<uint8_t> tests)
+	{ testToInt_t<uint8_t> tester; tester.testIntConversions(suite, tests); }
+void testToInt8(testsuit &suite, const testOk_t<int8_t> tests)
+	{ testToInt_t<int8_t> tester; tester.testIntConversions(suite, tests); }
+void testToUint16(testsuit &suite, const testOk_t<uint16_t> tests)
+	{ testToInt_t<uint16_t> tester; tester.testIntConversions(suite, tests); }
+void testToInt16(testsuit &suite, const testOk_t<int16_t> tests)
+	{ testToInt_t<int16_t> tester; tester.testIntConversions(suite, tests); }
+void testToUint32(testsuit &suite, const testOk_t<uint32_t> tests)
+	{ testToInt_t<uint32_t> tester; tester.testIntConversions(suite, tests); }
+void testToInt32(testsuit &suite, const testOk_t<int32_t> tests)
+	{ testToInt_t<int32_t> tester; tester.testIntConversions(suite, tests); }
+void testToUint64(testsuit &suite, const testOk_t<uint64_t> tests)
+	{ testToInt_t<uint64_t> tester; tester.testIntConversions(suite, tests); }
+void testToInt64(testsuit &suite, const testOk_t<int64_t> tests)
+	{ testToInt_t<int64_t> tester; tester.testIntConversions(suite, tests); }
+
 template<typename int_t> int_t swapBytes_(const int_t val) noexcept
 {
 	auto result(val);
