d793d278220775f2386dd3e73b62c741b2a0813f
dx-mon <dx-mon@users.sourceforge.net>
2017-11-12 00:24:17 +0000

    Lots more cleanup

--- test/conversions.cxx
+++ test/conversions.cxx
@@ -8,22 +8,17 @@ private:
 	using fromInt = fromInt_t<int_t, int_t>;
 
 public:
-	void testConversions(testsuit &suite, const testOk_t<int_t> &okTests, const testFailInt_t<int_t> &failTests)
+	void testConversions(testsuit &suite, const testOk_t<int_t> &tests)
 	{
-		for (const auto &test : okTests)
+		for (const auto &test : tests)
 		{
 			std::unique_ptr<char []> value = fromInt{test.first};
 			suite.assertEqual(value.get(), test.second);
 		}
-
-		for (const auto &test : failTests)
-		{
-			fromInt conv(test);
-		}
 	}
 };
 
-void testUint8(testsuit &suite, const testOk_t<uint8_t> &okTests, const testFailInt_t<uint8_t> &failTests)
-	{ testFromInt_t<uint8_t> tests; tests.testConversions(suite, okTests, failTests); }
-void testInt8(testsuit &suite, const testOk_t<int8_t> &okTests, const testFailInt_t<int8_t> &failTests)
-	{ testFromInt_t<int8_t> tests; tests.testConversions(suite, okTests, failTests); }
+void testUint8(testsuit &suite, const testOk_t<uint8_t> &okTests)
+	{ testFromInt_t<uint8_t> tests; tests.testConversions(suite, okTests); }
+void testInt8(testsuit &suite, const testOk_t<int8_t> &okTests)
+	{ testFromInt_t<int8_t> tests; tests.testConversions(suite, okTests); }
