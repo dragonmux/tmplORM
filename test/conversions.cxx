10b8580ad2f0a5176dde94801e71c7903547afdc
dx-mon <dx-mon@users.sourceforge.net>
2017-11-12 16:38:03 +0000

    Implemented a large template based harness to generate the fail tests for each of the numeric types in the conversions harness

--- test/conversions.cxx
+++ test/conversions.cxx
@@ -72,6 +72,67 @@ void testDecToUint64(testsuit &suite, const testOk_t<uint64_t> tests)
 void testDecToInt64(testsuit &suite, const testOk_t<int64_t> tests)
 	{ testToInt_t<int64_t> tester; tester.testIntConversions(suite, tests); }
 
+template<typename int_t> struct toIntType_t
+{
+private:
+	toInt_t<int_t> value;
+
+public:
+	toIntType_t(const char *const test) noexcept : value(test) { }
+	template<typename test_t> void test(testsuit &suite, const char *const test)
+	{
+		test_t what{suite, test};
+		what(value);
+	}
+};
+
+template<typename... int_t> struct toIntTypes_t;
+template<typename int_t, typename... ints_t> struct toIntTypes_t<int_t, ints_t...> : toIntTypes_t<ints_t...>
+{
+private:
+	toIntType_t<int_t> type;
+
+public:
+	toIntTypes_t(const char *const test) noexcept : toIntTypes_t<ints_t...>(test), type(test) { }
+
+	template<template<typename> class test_t> void test(testsuit &suite, const char *const test)
+	{
+		type.template test<test_t<toInt_t<int_t>>>(suite, test);
+		toIntTypes_t<ints_t...>::template test<test_t>(suite, test);
+	}
+};
+
+template<> struct toIntTypes_t<>
+{
+	toIntTypes_t(const char *const) noexcept { }
+	template<template<typename> class test_t> void test(testsuit &, const char *const) { }
+};
+
+template<typename toInt_t> struct testDecShouldFail_t
+{
+private:
+	testsuit &suite;
+	const char *const test;
+
+public:
+	testDecShouldFail_t(testsuit &suite_, const char *const test_) noexcept : suite(suite_), test(test_) { }
+
+	void operator ()(toInt_t &value)
+	{
+		suite.assertFalse(value.isInt());
+		suite.assertEqual(value.length(), str_t::length(test));
+	}
+};
+
+void testDecShouldFail(testsuit &suite, const testFailStr_t tests)
+{
+	for (const char *const test : tests)
+	{
+		toIntTypes_t<uint8_t, int8_t, uint16_t, int16_t, uint32_t, int32_t, uint64_t, int64_t> types{test};
+		types.template test<testDecShouldFail_t>(suite, test);
+	}
+}
+
 template<typename int_t> int_t swapBytes_(const int_t val) noexcept
 {
 	auto result(val);
--- test/testConversions.hxx
+++ test/testConversions.hxx
@@ -8,7 +8,7 @@ template<typename int_t> using testOkPair_t = std::pair<int_t, const char *const
 
 template<typename int_t> using testOk_t = std::vector<testOkPair_t<int_t>>;
 template<typename int_t> using testFailInt_t = std::vector<int_t>;
-using testFailStr_t = std::vector<const char *const>;
+using testFailStr_t = std::vector<const char *>;
 
 extern void testFromUint8(testsuit &suit, const testOk_t<uint8_t> tests);
 extern void testFromInt8(testsuit &suit, const testOk_t<int8_t> tests);
@@ -27,6 +27,7 @@ extern void testDecToUint32(testsuit &suit, const testOk_t<uint32_t> tests);
 extern void testDecToInt32(testsuit &suit, const testOk_t<int32_t> tests);
 extern void testDecToUint64(testsuit &suit, const testOk_t<uint64_t> tests);
 extern void testDecToInt64(testsuit &suit, const testOk_t<int64_t> tests);
+extern void testDecShouldFail(testsuit &suite, const testFailStr_t tests);
 
 extern uint16_t testSwapBytes(const uint16_t val) noexcept;
 extern uint32_t testSwapBytes(const uint32_t val) noexcept;
