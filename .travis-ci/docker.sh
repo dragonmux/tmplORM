1adb4a5bad5f38091c48da805b31ae9c2b097171
dx-mon <dx-mon@users.sourceforge.net>
2018-06-20 00:25:14 +0100

    Added scripts for setting up a test MSSQL instance for unit testing against and pointing our unit testing at it

---
+++ .travis-ci/docker.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+dir=$(readlink -f `dirname ${BASH_SOURCE[0]*/}`)
+mssqlPass=`$dir/makePassword.py`
+
+docker pull microsoft/mssql-server-linux:latest
+export MSSQL_CONTAINER=`docker run -e 'ACCEPT_EULA=Y' -e "SA_PASSWORD=$mssqlPass" -p 127.0.0.1:1433:1433/tcp \
+	--name mssql -d microsoft/mssql-server-linux:latest`
+
+export MSSQL_PASSWORD=$mssqlPass
+unset mssqlPass
