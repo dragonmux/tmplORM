8948b8be7c89b7a2e96ea27f53d315cbc85afd3f
dx-mon <dx-mon@users.sourceforge.net>
2018-06-20 00:45:48 +0100

    Added a couple more environment exports for the new docker script

--- .travis-ci/docker.sh
+++ .travis-ci/docker.sh
@@ -7,5 +7,7 @@ docker pull microsoft/mssql-server-linux:latest
 export MSSQL_CONTAINER=`docker run -e 'ACCEPT_EULA=Y' -e "SA_PASSWORD=$mssqlPass" -p 127.0.0.1:1433:1433/tcp \
 	--name mssql -d microsoft/mssql-server-linux:latest`
 
+export MSSQL_HOST=127.0.0.1
+export MSSQL_USERNAME=SA
 export MSSQL_PASSWORD=$mssqlPass
 unset mssqlPass
