c42b2c37a024a1d53b3b646932db60db6ec95fba
dx-mon <dx-mon@users.sourceforge.net>
2018-06-21 00:49:15 +0100

    ODBC is lower case in the package name.. consistency.. yes

--- .travis-ci/msodbcsql17.sh
+++ .travis-ci/msodbcsql17.sh
@@ -1,6 +1,6 @@
 wget https://packages.microsoft.com/keys/microsoft.asc -O - | sudo apt-key add -
 sudo wget https://packages.microsoft.com/config/ubuntu/14.04/prod.list -O /etc/apt/sources.list.d/mssql-release.list
 sudo apt-get update
-sudo ACCEPT_EULA=Y apt-get install msodbcsql17 unixODBC-dev
+sudo ACCEPT_EULA=Y apt-get install msodbcsql17 unixodbc-dev
 
 export MSSQL_DRIVER="ODBC Driver 17 for SQL Server"
