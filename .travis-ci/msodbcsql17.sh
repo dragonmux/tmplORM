754136add7eb2767e033daed3b3dafb06b8a3acd
dx-mon <dx-mon@users.sourceforge.net>
2018-06-21 01:33:05 +0100

    Another iteration of trying to get a modern enough unixODBC to install on Travis-CI

--- .travis-ci/msodbcsql17.sh
+++ .travis-ci/msodbcsql17.sh
@@ -2,6 +2,5 @@ wget https://packages.microsoft.com/keys/microsoft.asc -O - | sudo apt-key add -
 sudo wget https://packages.microsoft.com/config/ubuntu/14.04/prod.list -O /etc/apt/sources.list.d/mssql-release.list
 sudo apt-get update
 sudo ACCEPT_EULA=Y apt-get install msodbcsql17
-sudo apt-get install unixodbc-dev
 
 export MSSQL_DRIVER="ODBC Driver 17 for SQL Server"
