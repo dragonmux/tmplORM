f6c1a3df37073ae19f847ac943ef68ca2e8ef711
dx-mon <dx-mon@users.sourceforge.net>
2018-09-27 20:15:15 +0100

    Completed a first pass at getting the compiler to generate a JSON => model mapping

--- serializer/fromJSON.hxx
+++ serializer/fromJSON.hxx
@@ -4,6 +4,7 @@
 #include <rSON.h>
 #include <memory>
 #include <fixedVector.hxx>
+#include <tmplORM.hxx>
 #include <models.hxx>
 
 ////
@@ -45,5 +46,55 @@ public:
 	fixedVector_t<product_t> products() const noexcept;
 	fixedVector_t<customer_t> customers() const noexcept;
 };
+
+namespace tmplORM
+{
+	namespace json
+	{
+		using tmplORM::types::type_t;
+		using tmplORM::types::alias_t;
+		using tmplORM::common::toString;
+
+		template<typename value> struct lowerCamelCase : toString<value> { };
+		//template<char... C> struct lowerCamelCase<typestring<C...>> { };
+
+		template<typename...> struct populateFromJSON_t;
+		template<> struct populateFromJSON_t<>
+			{ template<typename T> static void populate(T &, const jsonObject_t &) { } };
+
+		template<typename field_t, typename... fields_t> struct populateFromJSON_t<field_t, fields_t...>
+		{
+			template<typename model_t, typename fieldName, typename T> static void populateField(model_t &model,
+				const jsonObject_t &data, const alias_t<fieldName, T> &)
+				{ model[fieldName()] = data[lowerCamelCase<fieldName>::value]; }
+
+			template<typename model_t, typename fieldName, typename T> static void populateField(model_t &model,
+				const jsonObject_t &data, const type_t<fieldName, T> &)
+				{ model[fieldName()] = data[lowerCamelCase<fieldName>::value]; }
+
+			template<typename T> static void populate(T &model, const jsonObject_t &data)
+			{
+				populateField(model, data, field_t());
+				populateFromJSON_t<fields_t...>::populate(model, data);
+			}
+		};
+
+		template<typename T, typename... fields_t> struct modelFromJSON_t
+		{
+			static T convert(const jsonObject_t &object)
+			{
+				T model{};
+				populateFromJSON_t<fields_t...>::populate(model, object);
+				return std::move(model);
+			}
+		};
+
+		template<typename T, typename tableName, typename... fields_t> T modelFromJSON(const jsonObject_t &object,
+			const model_t<tableName, fields_t...> &) { return modelFromJSON_t<T, fields_t...>::convert(object); }
+	}
+
+	template<typename T> T modelFromJSON(const jsonObject_t &data)
+		{ return tmplORM::json::modelFromJSON<T>(data, T()); }
+}
 
 #endif /*FROM_JSON__HXX*/
