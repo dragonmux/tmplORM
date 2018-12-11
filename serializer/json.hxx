c08204cfbc9ed1545b05baae079e0904f7a6ef02
dx-mon <dx-mon@users.sourceforge.net>
2018-12-11 22:42:38 +0000

    Implemented a type for handling nullable fields that also need being able to be converted to and from JSON

--- serializer/json.hxx
+++ serializer/json.hxx
@@ -25,4 +25,67 @@ protected:
 using rSON::typeIs;
 using rSON::typeIsOrNull;
 
+namespace tmplORM
+{
+	namespace types
+	{
+		namespace json
+		{
+			template<typename nullable_t, bool =
+				std::is_same<typename std::remove_const<typename nullable_t::type>::type, char *>::value ||
+				std::is_same<typename nullable_t::type, tmplORM::types::ormDateTime_t>::value ||
+				std::is_same<typename nullable_t::type, tmplORM::types::ormDate_t>::value>
+				struct nullableValue_t
+			{
+				using type = typename nullable_t::type;
+				void operator ()(nullable_t &container, const jsonAtom_t &_value) const
+				{
+					if (_value.isNull())
+						container.value(nullptr);
+					else
+					{
+						const type rawValue = _value;
+						container.value(rawValue);
+					}
+				}
+			};
+
+			template<typename nullable_t> struct nullableValue_t<nullable_t, true>
+			{
+				using type = typename nullable_t::type;
+				void operator ()(nullable_t &container, const jsonAtom_t &_value) const
+				{
+					if (_value.isNull())
+						container.value(nullptr);
+					else
+					{
+						const char *const rawValue = _value;
+						container.value(const_cast<char *>(rawValue));
+					}
+				}
+			};
+		}
+
+		template<typename T> struct jsonNullable_t : public nullable_t<T>
+		{
+		private:
+			using nullableValue_t = tmplORM::types::json::nullableValue_t<nullable_t<T>>;
+
+		public:
+			using type = typename nullable_t<T>::type;
+			using nullable_t<T>::operator =;
+			using nullable_t<T>::value;
+			using nullable_t<T>::operator ==;
+			using nullable_t<T>::operator !=;
+
+			void operator =(const jsonAtom_t &_value) noexcept { value(_value); }
+			void value(const jsonAtom_t &_value)
+			{
+				const nullableValue_t setter{};
+				setter(*this, _value);
+			}
+		};
+	}
+}
+
 #endif /*tmplORM_JSON__HXX*/
