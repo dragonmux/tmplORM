de91795c3bacd17c5aa97fe562667b014ee2d51b
dx-mon <dx-mon@users.sourceforge.net>
2019-08-18 20:38:45 +0100

    Fixed the version of msodbcsql17 the tests depend on as we're not on Trusty any more

--- .travis-ci/msodbcsql17.sh
+++ .travis-ci/msodbcsql17.sh
@@ -1,5 +1,5 @@
 wget https://packages.microsoft.com/keys/microsoft.asc -O - | sudo apt-key add -
-sudo wget https://packages.microsoft.com/config/ubuntu/14.04/prod.list -O /etc/apt/sources.list.d/mssql-release.list
+sudo wget https://packages.microsoft.com/config/ubuntu/16.04/prod.list -O /etc/apt/sources.list.d/mssql-release.list
 sudo apt-get update
 sudo ACCEPT_EULA=Y apt-get install msodbcsql17 unixodbc-dev
 
