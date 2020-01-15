387bc13cea2431c0eb3900ba8e0444568ef68430
dx-mon <dx-mon@users.sourceforge.net>
2020-01-15 21:02:28 +0000

    Fixed the copyright in build64.sh

--- build64.sh
+++ build64.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # This file is part of rSON
-# Copyright © 2012-2013 Rachel Mant (dx-mon@users.sourceforge.net)
+# Copyright © 2012-2019 Rachel Mant (dx-mon@users.sourceforge.net)
 #
 # rSON is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Lesser General Public License as published by
@@ -14,5 +14,4 @@
 #
 # You should have received a copy of the GNU Lesser General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
 GCC="gcc -m64 -fPIC -DPIC" GXX="g++ -m64 -fPIC -DPIC" LIBDIR=/usr/lib64 make "$@"
