141a42cfda9f6b9a50473e32c1d647de312ff970
dragonmux <git@dragonmux.network>
2022-01-31 05:14:33 -0500

    Implemented value retrieval and the first of the conversion functions for Postgres data

--- pgsql.cxx
+++ pgsql.cxx
@@ -1,4 +1,7 @@
 #include <utility>
+#include <substrate/index_sequence>
+// AAAAAAAAGGGGHHH.. this should be in the libpq headers, but no distro puts it where they should.
+#include <catalog/pg_type_d.h>
 #include "pgsql.hxx"
 
 /*!
@@ -37,8 +40,18 @@ bool pgSQLClient_t::connect(const char *const host, const char *const port, cons
 pgSQLResult_t pgSQLClient_t::query(const char *const queryStmt) const noexcept
 	{ return {PQexec(connection, queryStmt)}; }
 
-pgSQLResult_t::pgSQLResult_t(pgSQLResult_t &&res) noexcept : pgSQLResult_t()
-	{ std::swap(result, res.result); }
+pgSQLResult_t::pgSQLResult_t(PGresult *res) noexcept : result{res}
+{
+	if (!result)
+		return;
+	fields = static_cast<uint32_t>(PQnfields(result));
+	fieldInfo = {fields};
+	if (!fieldInfo.valid())
+		return;
+	for (const auto i : substrate::indexSequence_t{fields})
+		// This would use fieldInfo[i] but that can throw.
+		fieldInfo.data()[i] = PQftype(result, static_cast<int>(i));
+}
 
 pgSQLResult_t::~pgSQLResult_t() noexcept
 {
@@ -64,3 +77,87 @@ bool pgSQLResult_t::successful() const noexcept
 }
 
 uint32_t pgSQLResult_t::numRows() const noexcept { return valid() ? static_cast<uint32_t>(PQntuples(result)) : 0; }
+
+bool pgSQLResult_t::next() noexcept
+{
+	++row;
+	return false;
+}
+
+pgSQLValue_t pgSQLResult_t::operator [](const uint32_t idx) const noexcept
+{
+	if (idx > fields || !valid())
+		return {};
+	else if (PQgetisnull(result, static_cast<int>(row), static_cast<int>(idx)) == 1)
+		return {nullptr};
+	else
+		// This would use fieldInfo[i] but that can throw.
+		return {PQgetvalue(result, static_cast<int>(row), static_cast<int>(idx)), fieldInfo.data()[idx]};
+}
+
+void pgSQLResult_t::swap(pgSQLResult_t &res) noexcept
+{
+	std::swap(result, res.result);
+	std::swap(fields, res.fields);
+	std::swap(row, res.row);
+	fieldInfo.swap(res.fieldInfo);
+}
+
+pgSQLValue_t::pgSQLValue_t(std::nullptr_t) noexcept : type{ANYOID} { }
+
+template<typename T> T pgSQLValue_t::reinterpret() const noexcept
+{
+	T value{};
+	memcpy(&value, data, sizeof(T));
+	return value;
+}
+
+bool pgSQLValue_t::asBool() const
+{
+	if (isNull() || type != BOOLOID)
+		throw pgSQLValueError_t{pgSQLErrorType_t::boolError};
+	const auto value = reinterpret<uint8_t>();
+	return value != 0;
+}
+
+const char *pgSQLValueError_t::error() const noexcept
+{
+	switch (errorType)
+	{
+		case pgSQLErrorType_t::noError:
+			return "No error occured";
+		case pgSQLErrorType_t::stringError:
+			return "Error converting value to a string";
+		case pgSQLErrorType_t::boolError:
+			return "Error converting value to a boolean";
+		case pgSQLErrorType_t::uint8Error:
+			return "Error converting value to an unsigned 8-bit integer";
+		case pgSQLErrorType_t::int8Error:
+			return "Error converting value to a signed 8-bit integer";
+		case pgSQLErrorType_t::uint16Error:
+			return "Error converting value to an unsigned 16-bit integer";
+		case pgSQLErrorType_t::int16Error:
+			return "Error converting value to a signed 16-bit integer";
+		case pgSQLErrorType_t::uint32Error:
+			return "Error converting value to an unsigned 32-bit integer";
+		case pgSQLErrorType_t::int32Error:
+			return "Error converting value to a signed 32-bit integer";
+		case pgSQLErrorType_t::uint64Error:
+			return "Error converting value to an unsigned 64-bit integer";
+		case pgSQLErrorType_t::int64Error:
+			return "Error converting value to a signed 64-bit integer";
+		case pgSQLErrorType_t::floatError:
+			return "Error converting value to a single-precision floating point number";
+		case pgSQLErrorType_t::doubleError:
+			return "Error converting value to a double-precision floating point number";
+		case pgSQLErrorType_t::binError:
+			return "Error converting value to binary buffer";
+		case pgSQLErrorType_t::dateError:
+			return "Error converting value to a date quantity";
+		case pgSQLErrorType_t::dateTimeError:
+			return "Error converting value to a date and time quantity";
+		case pgSQLErrorType_t::uuidError:
+			return "Error converting value to a UUID";
+	}
+	return "An unknown error occured";
+}
--- pgsql.hxx
+++ pgsql.hxx
@@ -18,27 +18,70 @@ namespace tmplORM
 	{
 		namespace driver
 		{
+using namespace tmplORM::types::baseTypes;
+
+struct tmplORM_API pgSQLValue_t final
+{
+private:
+	const void *data{nullptr};
+	Oid type{InvalidOid};
+
+	template<typename T> T reinterpret() const noexcept;
+
+public:
+	/*! @brief Default constructor for value objects, constructing the invalid value by default */
+	pgSQLValue_t() noexcept = default;
+	pgSQLValue_t(std::nullptr_t) noexcept;
+	pgSQLValue_t(const char *const value, Oid _type) noexcept : data{static_cast<const void *>(value)}, type{_type} { }
+
+	bool valid() const noexcept;
+	bool isNull() const noexcept { return !data; }
+	bool asBool() const;
+	uint8_t asUint8() const;
+	int8_t asInt8() const;
+	uint16_t asUint16() const;
+	int16_t asInt16() const;
+	uint32_t asUint32() const;
+	int32_t asInt32() const;
+	uint64_t asUint64() const;
+	int64_t asInt64() const;
+	float asFloat() const;
+	double asDouble() const;
+	ormDate_t asDate() const;
+	ormDateTime_t asDateTime() const;
+	ormUUID_t asUUID() const;
+};
+
 struct tmplORM_API pgSQLResult_t final
 {
 private:
 	PGresult *result{nullptr};
+	uint32_t fields{0};
+	uint32_t row{0};
+	// NOLINTNEXTLINE(cppcoreguidelines-avoid-c-arrays,modernize-avoid-c-arrays)
+	fixedVector_t<Oid> fieldInfo{};
 
 public:
 	/*! @brief Default constructor for result objects, constructing invalid result objects by default */
 	constexpr pgSQLResult_t() noexcept = default;
-	pgSQLResult_t(PGresult *res) noexcept : result{res} { }
-	pgSQLResult_t(pgSQLResult_t &&res) noexcept;
+	pgSQLResult_t(PGresult *res) noexcept;
+	pgSQLResult_t(pgSQLResult_t &&res) noexcept : pgSQLResult_t{} { swap(res); }
 	~pgSQLResult_t() noexcept;
 	pgSQLResult_t &operator =(pgSQLResult_t &&res) noexcept;
 	/*!
 	 * @brief Call to determine if this result object is valid
 	 * @returns true if the object is valid, false otherwise
 	 */
-	bool valid() const noexcept { return result; }
+	bool valid() const noexcept { return result && (!fields || fieldInfo.valid()); }
 	uint32_t errorNum() const noexcept;
 	const char *error() const noexcept;
 	bool successful() const noexcept;
 	uint32_t numRows() const noexcept;
+	uint32_t numFields() const noexcept { return fields; }
+	bool next() noexcept;
+	pgSQLValue_t operator [](const uint32_t idx) const noexcept;
+
+	void swap(pgSQLResult_t &res) noexcept;
 
 	/*! @brief Deleted copy constructor for pgSQLResult_t as results are not copyable */
 	pgSQLResult_t(const pgSQLResult_t &) = delete;
@@ -68,6 +111,39 @@ public:
 	pgSQLClient_t(const pgSQLClient_t &) = delete;
 	/*! @brief Deleted move assignment operator for pgSQLClient_t as client connections are not movable */
 	pgSQLClient_t &operator =(const pgSQLClient_t &) = delete;
+};
+
+enum class pgSQLErrorType_t : uint8_t
+{
+	noError, binError,
+	stringError, boolError,
+	uint8Error, int8Error,
+	uint16Error, int16Error,
+	uint32Error, int32Error,
+	uint64Error, int64Error,
+	floatError, doubleError,
+	dateError, dateTimeError,
+	uuidError
+};
+
+struct tmplORM_API pgSQLValueError_t final : std::exception
+{
+private:
+	pgSQLErrorType_t errorType{pgSQLErrorType_t::noError};
+
+public:
+	pgSQLValueError_t() noexcept = default;
+	pgSQLValueError_t(const pgSQLErrorType_t type) noexcept : errorType{type} { }
+	pgSQLValueError_t(const pgSQLValueError_t &) noexcept = default;
+	pgSQLValueError_t(pgSQLValueError_t &&) noexcept = default;
+	~pgSQLValueError_t() noexcept final = default;
+	pgSQLValueError_t &operator =(const pgSQLValueError_t &) noexcept = default;
+	pgSQLValueError_t &operator =(pgSQLValueError_t &&) noexcept = default;
+	const char *error() const noexcept;
+	const char *what() const noexcept final { return error(); }
+
+	bool operator ==(const pgSQLValueError_t &error) const noexcept { return errorType == error.errorType; }
+	bool operator !=(const pgSQLValueError_t &error) const noexcept { return errorType != error.errorType; }
 };
 		} // namespace driver
 	} // namespace pgsql
