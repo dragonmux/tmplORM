cefe27754cfc8a53b659d7bdcc8f804b9004df87
dragonmux <git@dragonmux.network>
2022-02-02 21:22:12 -0500

    Removed the now redundant msodbcsql17 installation script for Travis-CI

--- .travis-ci/msodbcsql17.sh
+++
@@ -1,6 +0,0 @@
-wget https://packages.microsoft.com/keys/microsoft.asc -O - | sudo apt-key add -
-sudo wget https://packages.microsoft.com/config/ubuntu/16.04/prod.list -O /etc/apt/sources.list.d/mssql-release.list
-sudo apt-get update
-sudo ACCEPT_EULA=Y apt-get install msodbcsql17 unixodbc-dev
-
-export MSSQL_DRIVER="ODBC Driver 17 for SQL Server"
